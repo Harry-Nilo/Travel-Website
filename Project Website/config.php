253c4174db5674fc9eece80705e8218e4ac9e0e8
Harry B. Nilo <niloharryb07@gmail.com>
2025-05-11 21:01:30 +0800

    Update config.php

--- Project Website/config.php
+++ Project Website/config.php
@@ -2,11 +2,10 @@
 $host = 'localhost';
 $db = 'user_auth';
 $user = 'root';
-$pass = ''; // Leave this blank for XAMPP unless you've set a password
+$pass = ''; 
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
