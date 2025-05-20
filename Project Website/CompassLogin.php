f930dc3dfe2647fd9aeab26c4a24bfcfe65e1a4a
Harry B. Nilo <niloharryb07@gmail.com>
2025-05-20 11:27:26 +0800

    Update CompassLogin.php

--- Project Website/CompassLogin.php
+++ Project Website/CompassLogin.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Compass Login</title>
-    <link rel="stylesheet" href="CompassLogin1.css">
+    <link rel="stylesheet" href="CompassLogin.css">
 </head>
 <body>
     <div class="container">
@@ -10,14 +12,14 @@
             <h2>Login to Compass Travel</h2>
             <input type="text" name="username_email" placeholder="Username or Email" required>
             <div class="password-container">
-    <input type="password" name="password" id="password" placeholder="Password" required>
-    <button type="button" class="show-password" onclick="togglePassword()">Show</button>
-</div>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <button type="button" class="show-password" onclick="togglePassword()">Show</button>
+            </div>
             <button type="submit">Login</button>
             <p>Forgot your password? <a href="CompassForgetPass.php">Reset Password</a></p>
             <p>Don't have an account? <a href="CompassRegister.php">Register here</a></p>
         </form>
     </div>
-<script src="CompassLogin.js"></script>
+    <script src="CompassLogin.js"></script>
 </body>
 </html>
