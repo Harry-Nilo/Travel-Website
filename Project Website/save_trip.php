355571b20235d946bd0c3cb44d6309c441a68255
Guillmar19 <guillmarmayagma10@gmail.com>
2025-05-23 23:38:52 +0800

    Update save_trip.php

--- Project Website/save_trip.php
+++ Project Website/save_trip.php
@@ -6,30 +6,24 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-// Database credentials
 $host = "localhost";
 $user = "root";
 $password = "";
 $dbname = "compass_site";
 
-// Connect to MySQL
 $conn = new mysqli($host, $user, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get user ID from session
 $user_id = $_SESSION['user_id'];
 
-// Get POST data
 $city = $_POST['city'] ?? '';
 $country = $_POST['country'] ?? '';
 $activities = isset($_POST['activity']) ? implode(', ', $_POST['activity']) : '';
 $info_types = isset($_POST['info']) ? implode(', ', $_POST['info']) : '';
 
-// Insert into database
 $stmt = $conn->prepare("INSERT INTO plans (city, country, activities, info_types, user_id) VALUES (?, ?, ?, ?, ?)");
 $stmt->bind_param("ssssi", $city, $country, $activities, $info_types, $user_id);
 
@@ -42,7 +36,6 @@ if ($stmt->execute()) {
 $stmt->close();
 $conn->close();
 ?>
-
 
 
 
